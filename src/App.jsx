09dd1c951b0dc35b864c4376a248d75d827dd9cd
Manu14 <manuelru2008@gmail.com>
2025-12-02 19:03:44 +0100

    Update 2App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -738,7 +738,8 @@ const AlberoCittadinanza = () => {
             <br />
             <span className="text-sm md:text-base">- Nelson Mandela</span>
           </p>
-          <p className="mt-4 text-gray-700 font-semibold text-sm md:text-base">Classe 4CIT - Trimestre 2025/2026 by Ruscitto M. & Pernarella M.</p>
+          <p className="mt-4 text-gray-700 font-semibold text-sm md:text-base">Classe 4CIT - Trimestre 2025/2026 </p>
+          <p className="mt-4 text-gray-700 font-semibold text-sm md:text-base">by Ruscitto M. & Pernarella M.</p>
         </div>
 
         <div className="bg-gradient-to-b from-sky-50 to-green-50 rounded-2xl shadow-2xl p-2 md:p-8">
