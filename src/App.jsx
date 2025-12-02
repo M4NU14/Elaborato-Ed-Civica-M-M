7e693eef4dcc197ae98a7a6bb2bb98de52f838f0
Manu14 <manuelru2008@gmail.com>
2025-12-02 18:04:05 +0100

    Update 2App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -690,8 +690,8 @@ const AlberoCittadinanza = () => {
         
         @media (max-width: 768px) {
           .fruit-icon {
-            width: 32x !important;
-            height: 32px !important;
+            width: 35x !important;
+            height: 35px !important;
             border-width: 2px !important;
           }
           .fruit-icon span {
